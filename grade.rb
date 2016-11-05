33568ac44e91610106d3c9ab8938e3afee0b97fb
ran010 <ranjanbajra@gmail.com>
2016-11-05 23:08:30 +0545

    Create grade.rb

---
+++ grade.rb
@@ -0,0 +1,16 @@
+def grader(score)
+  # TODO: complete me
+if score > 1.0
+    'F'
+  elsif score >= 0.9
+    'A'
+  elsif score >= 0.8
+    'B'
+  elsif score >= 0.7
+    'C'
+  elsif score >= 0.6
+    'D'
+  else
+    'F'
+  end
+end
